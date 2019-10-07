efaa7f9fba5daea3f29aa26c1223594428e60e71
Andrew Tam <tam.andrew7@gmail.com>
2019-10-07 17:40:31 +0200

    Start experimenting with front-end

--- accumulator/src/lib.rs
+++ accumulator/src/lib.rs
@@ -4,7 +4,8 @@ pub mod subroutines;
 pub mod proofs;
 pub mod witnesses;
 
-// Defines the RSA group.
+// Defines the RSA group. Currently uses a temporary value for testing.
 pub const MODULUS: u64 = 13;
+
 // Security parameter that represents the size of elements added to the accumulator.
 pub const LAMBDA: u64 = u64::max_value()/2;
